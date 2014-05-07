6b773bff490be9cc3b2ceb478ec2196fe2c8cc8f
Zach Jacobs <zachjacobs@gmail.com>
2014-05-07 15:28:31 -0400

    Initial commit.  Console application uses reflection to get the version number of an assembly.

---
+++ AssemblyVersion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyVersion
+{
+    class Program
+    {
+        /// <summary>
+        /// This is designed to be a console application that accepts the path to an assembly file as the first argument.
+        /// Reflection is used to write the Version information to the console.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Assembly asm = Assembly.LoadFrom(args[0]);
+            Console.WriteLine(asm.GetName().Version);
+        }
+    }
+}
