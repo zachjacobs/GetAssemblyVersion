8f6f774f67d9f0c5f21763d59415c6d02c4ccf84
Zach Jacobs <zachjacobs@gmail.com>
2015-01-28 09:01:20 -0500

    Adds support for a full version string.

--- AssemblyVersion/Program.cs
+++ AssemblyVersion/Program.cs
@@ -11,13 +11,35 @@ namespace AssemblyVersion
     {
         /// <summary>
         /// This is designed to be a console application that accepts the path to an assembly file as the first argument.
+        /// The second argument can either be omitted or can be a full to use the full version number.
         /// Reflection is used to write the Version information to the console.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            if (!args.Any())
+            {
+                Console.WriteLine("AssemblyVersion.exe <path to assembly> \"-full\", use optional full for the full version number: <major>.<minor>.<build>.<revision>");
+                return;
+            }
+
             Assembly asm = Assembly.LoadFrom(args[0]);
-            Console.WriteLine(asm.GetName().Version.Major + "." + asm.GetName().Version.Minor + "." + asm.GetName().Version.Build);
+
+            try
+            {
+                //use the full version number <major>.<minor>.<build>.<revision>
+                if (args.Count() > 1 && String.Equals(args[1].TrimStart('-'), "full"))
+                {
+                    Console.WriteLine(asm.GetName().Version);
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+            }
+
+            //use the old style version number <major>.<minor>.<build>
+            Console.WriteLine(asm.GetName().Version.Major + "." + asm.GetName().Version.Minor + "." + asm.GetName().Version.Build);    
         }
     }
 }
